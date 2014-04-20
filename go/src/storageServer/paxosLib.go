ed62c12b31f1d8f6cdd2776c886ff84c2a43a4c9
ibukunadeleye <iia@andrew.cmu.edu>
2014-04-20 19:08:14 -0400

    committing lib

---
+++ go/src/storageServer/paxosLib.go
@@ -0,0 +1,30 @@
+package storageServer
+
+import (
+	"fmt"
+)
+
+type PreparePkg struct{
+	N int
+	CS int
+	HostPort string
+}
+
+type AcceptPkg struct{
+	N int
+	V []byte
+	CS int	
+	HostPort string
+}
+
+type CommitPkg struct{
+	N int
+	V []byte
+	hostport string
+}
+
+type Reply struct{
+	ok bool
+	value []byte
+	CS int
+}
